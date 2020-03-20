377a70b99402c62eddc1789cbaefda0b5847f9b1
Martin Kröner <kronerm9@gmail.com>
2020-03-20 19:52:11 +0100

    Idk something something tweaks

--- bspwm/.config/bspwm/bspwm_colors.sh
+++ bspwm/.config/bspwm/bspwm_colors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#6e504c"
-bspc config normal_border_color  "#4b3734"
+bspc config focused_border_color "#8A645F"
+bspc config normal_border_color  "#3A4861"
