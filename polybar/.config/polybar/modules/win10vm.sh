0d251358dcbfb6ef8631b0621d0d238b20b1a947
Martin Kröner <kronerm9@gmail.com>
2021-06-20 21:51:47 +0200

    polybar: win10vm: remove sudo, format

--- polybar/.config/polybar/modules/win10vm.sh
+++ polybar/.config/polybar/modules/win10vm.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-
-
-WIN10_STATE=$(sudo virsh list --all | awk '/win10/ {print $3}');
+WIN10_STATE=$(virsh list --all | awk '/win10/ {print $3}')
 
 if [[ $WIN10_STATE = "running" ]]; then
-	echo " W10 VM running";
+	echo " W10 VM running"
 else
-	echo "";
+	echo ""
 fi
-
