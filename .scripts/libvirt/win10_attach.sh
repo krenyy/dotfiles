9e49d3b4f077f5a7a8bc96610840ff89503ec970
Martin Kröner <kronerm9@gmail.com>
2022-03-07 13:08:07 +0100

    libvirt: remove scripts as i no longer use win10 vm

--- .scripts/libvirt/win10_attach.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-virsh attach-device win10 kb.xml &&
-	virsh attach-device win10 mouse.xml &&
-	ddcutil setvcp 60 0x12
--- .scripts/libvirt/win10_detach.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-virsh detach-device win10 kb.xml &&
-	virsh detach-device win10 mouse.xml &&
-	ddcutil setvcp 60 0x0f
