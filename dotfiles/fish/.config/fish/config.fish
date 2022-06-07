9dd80e47bd35b91de44615d620110563d89faf95
Martin Kröner <kronerm9@gmail.com>
2022-06-07 12:49:26 +0200

    switch from zsh to fish

---
+++ dotfiles/fish/.config/fish/config.fish
@@ -0,0 +1,19 @@
+function fish_greeting
+    # Empty function to suppress the default message
+end
+
+if status is-interactive
+    alias cp="cp -i"
+    alias df="df -h"
+    alias ip='ip -c'
+    alias ls='exa -alH --group-directories-first'
+    alias mv='mv -i'
+    alias rm='rm -i'
+    alias wget='wget --hsts-file /dev/null'
+
+    alias sprunge='curl -F "sprunge=<-" http://sprunge.us'
+
+    if command -v pazi &>/dev/null
+        eval "$(pazi init fish)"
+    end
+end
