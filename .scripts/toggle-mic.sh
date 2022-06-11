04737577eb0f7c41594dbe58046edb1ad8743253
Martin Kröner <kronerm9@gmail.com>
2022-06-11 22:24:06 +0200

    scripts: change toggle_mic sound files

--- .scripts/toggle-mic.sh
+++ .scripts/toggle-mic.sh
@@ -9,12 +9,12 @@ MUTED="$?"
 
 if [ "$MUTED" == "0" ]; then
   MESSAGE=" muted"
-  SOUND_FILENAME="micmute.wav"
+  SOUND_FILENAME="message.oga"
 else
   MESSAGE=" unmuted"
-  SOUND_FILENAME="micunmute.wav"
+  SOUND_FILENAME="bell.oga"
 fi
 
 $AMIXER_CMD set Mic toggle >/dev/null &&
   notify-send.py "$MESSAGE" --hint boolean:transient:true -t 1000 --replaces-process "mic-toggle-popup" &&
-  DISPLAY=:0 mpv "$HOME/.local/share/sounds/$SOUND_FILENAME" >/dev/null
+  DISPLAY=:0 mpv "/usr/share/sounds/freedesktop/stereo/$SOUND_FILENAME" >/dev/null
