24c30789486fba9b72e8306370bc2fd31b0e21f7
Martin Kröner <kronerm9@gmail.com>
2022-08-16 13:21:31 +0200

    get rid of alacritty opengl workaround (no longer use the device)

--- .scripts/alacritty-glsl-workaround.sh
+++
@@ -1,3 +0,0 @@
-GLSL_MAX_VERSION_SUPPORTED=$(glxinfo | awk '/OpenGL version/ {print $4}')
-UNSUPPORTED=$(echo "$GLSL_MAX_VERSION_SUPPORTED < 3.3" | bc -l)
-LIBGL_ALWAYS_SOFTWARE=$UNSUPPORTED alacritty $@
