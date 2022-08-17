2df4b0c6b6201f0524073e7b54621745e88ca9e0
Martin Kröner <kronerm9@gmail.com>
2022-08-17 08:40:37 +0200

    neovim: bootstrap packer

--- dotfiles/neovim/.config/nvim/lua/plugins/init.lua
+++ dotfiles/neovim/.config/nvim/lua/plugins/init.lua
@@ -1,3 +1,10 @@
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	packer_bootstrap =
+		vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	vim.cmd([[packadd packer.nvim]])
+end
+
 require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 
