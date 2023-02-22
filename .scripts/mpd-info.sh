547828e2b6bf63a9f377473987369ac8ccb03cf2
Martin Kröner <kronerm9@gmail.com>
2023-02-22 17:41:17 +0100

    scripts: mpd-info: fix for make notification daemon

--- .scripts/mpd-info.sh
+++ .scripts/mpd-info.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 TRACK_TITLE="$(mpc current -f "%title%")"
-if [ "$(echo "$TRACK_TITLE" | wc -c)" -le 1 ]; then
-  TRACK_TITLE="$(basename $(mpc current))"
+if [ "${#TRACK_TITLE}" -le 1 ]; then
+  TRACK_TITLE="$(basename "$(mpc current)")"
 fi
 TRACK_PERCENTAGE="$(mpc status %percenttime% | xargs | sed 's/%//')"
 TIME_ELAPSED="$(mpc status "%currenttime%/%totaltime%")"
 MPD_VOLUME="$(mpc volume | cut -d':' -f2 | xargs)"
 
-notify-send.py "$TRACK_TITLE" "$TIME_ELAPSED $MPD_VOLUME" \
-  --hint boolean:transient:true int:has-percentage:"$TRACK_PERCENTAGE" -t 2000 \
-  --replaces-process "mpd-info-popup"
+notify-send -a "$TRACK_TITLE" "$TIME_ELAPSED - $MPD_VOLUME" \
+  -t 1000 \
+  -e \
+  --hint int:value:"$TRACK_PERCENTAGE" \
+  --hint string:x-dunst-stack-tag:mpd
