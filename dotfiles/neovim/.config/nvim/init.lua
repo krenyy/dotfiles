18b8b25f576afd6c053cda2c6edec598c761f193
Martin Kröner <kronerm9@gmail.com>
2023-04-07 12:27:39 +0200

    neovim: replace feline with lualine

--- dotfiles/neovim/.config/nvim/init.lua
+++ dotfiles/neovim/.config/nvim/init.lua
@@ -484,9 +484,34 @@ local plugins = {
 		end,
 	},
 	{
-		"freddiehaddad/feline.nvim",
+		"nvim-lualine/lualine.nvim",
 		config = function()
-			require("feline").setup()
+			local noice = require("noice")
+			require("lualine").setup({
+				sections = {
+					lualine_x = {
+						{
+							noice.api.status.message.get_hl,
+							cond = noice.api.status.message.has,
+						},
+						{
+							noice.api.status.command.get,
+							cond = noice.api.status.command.has,
+							color = { fg = "#ff9e64" },
+						},
+						{
+							noice.api.status.mode.get,
+							cond = noice.api.status.mode.has,
+							color = { fg = "#ff9e64" },
+						},
+						{
+							noice.api.status.search.get,
+							cond = noice.api.status.search.has,
+							color = { fg = "#ff9e64" },
+						},
+					},
+				},
+			})
 		end,
 	},
 	{
