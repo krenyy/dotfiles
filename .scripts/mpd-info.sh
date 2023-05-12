eada8028fb8fed4843d71fe43b3b52ae44e9f334
Martin Kröner <kronerm9@gmail.com>
2023-05-12 21:44:33 +0200

    scripts: mpd-info: formatting, delay change

--- .scripts/mpd-info.sh
+++ .scripts/mpd-info.sh
@@ -2,14 +2,14 @@
 
 TRACK_TITLE="$(mpc current -f "%title%")"
 if [ "${#TRACK_TITLE}" -le 1 ]; then
-  TRACK_TITLE="$(basename "$(mpc current)")"
+	TRACK_TITLE="$(basename "$(mpc current)")"
 fi
 TRACK_PERCENTAGE="$(mpc status %percenttime% | xargs | sed 's/%//')"
 TIME_ELAPSED="$(mpc status "%currenttime%/%totaltime%")"
 MPD_VOLUME="$(mpc volume | cut -d':' -f2 | xargs)"
 
 notify-send -a "$TRACK_TITLE" "$TIME_ELAPSED - $MPD_VOLUME" \
-  -t 1000 \
-  -e \
-  --hint int:value:"$TRACK_PERCENTAGE" \
-  --hint string:x-dunst-stack-tag:mpd
+	-t 2000 \
+	-e \
+	--hint int:value:"$TRACK_PERCENTAGE" \
+	--hint string:x-dunst-stack-tag:mpd
