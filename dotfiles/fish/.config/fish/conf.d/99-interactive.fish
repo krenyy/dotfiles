7009874111586c52a34224c2bc3ebed4ebf6aa6e
Martin Kröner <kronerm9@gmail.com>
2023-08-07 01:07:51 +0200

    better `rm` alias

--- dotfiles/fish/.config/fish/conf.d/99-interactive.fish
+++ dotfiles/fish/.config/fish/conf.d/99-interactive.fish
@@ -4,7 +4,7 @@ if status is-interactive
     alias ip='ip -c'
     alias ls='exa -alH --group-directories-first'
     alias mv='mv -i'
-    alias rm='rm -i'
+    alias rm='rm -I'
     alias e='emacsclient -c -nw --alternate-editor hx'
 
     if command -v pazi &>/dev/null
