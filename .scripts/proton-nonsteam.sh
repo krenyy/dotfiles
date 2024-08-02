c7b8ff59bbf5dc7de94dc78116b43d456a0517b9
Martin Kröner <work@kroner.dev>
2024-08-02 22:53:18 +0200

    scripts: proton-nonsteam: rofi -> fzf

--- .scripts/proton-nonsteam.sh
+++ .scripts/proton-nonsteam.sh
@@ -69,7 +69,7 @@ ln -fnrs "$HOME_DIR" "$STEAMUSER_DIR"
 EXE_DIR="$(dirname "$EXE_PATH")"
 EXE="$(basename "$EXE_PATH")"
 
-GAMESCOPE="gamescope -b $(cat /sys/class/drm/*/modes | sort -h | uniq | rofi -dmenu | sed -E 's/^/-W /;s/x/ -H /') -r $(printf '30\n60\n72\n90\n120\n144\n165\n240' | rofi -dmenu) --"
+GAMESCOPE="gamescope -b $(cat /sys/class/drm/*/modes | sort -h | uniq | fzf | sed -E 's/^/-W /;s/x/ -H /') -r $(printf '30\n60\n72\n90\n120\n144\n165\n240' | fzf) --"
 [ "$USE_GAMESCOPE" -eq 0 ] && GAMESCOPE=""
 
 case "$1" in
