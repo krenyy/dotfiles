2679136de8ca55597aa8b47dd4b4cf4f4fe28780
Martin Kröner <work@kroner.dev>
2024-08-10 18:08:55 +0200

    waybar: power_draw: cleanup

--- dotfiles/waybar/.config/waybar/power_draw.rs
+++ dotfiles/waybar/.config/waybar/power_draw.rs
@@ -2,17 +2,14 @@
 
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     const BATTERY_DIR: &str = "/sys/class/power_supply/BAT0";
-    let current_now = fs::read_to_string(format!("{BATTERY_DIR}/current_now"))
-        .unwrap()
+    let current_now = fs::read_to_string(format!("{BATTERY_DIR}/current_now"))?
         .trim()
-        .parse::<f64>()
-        .unwrap();
-    let voltage_now = fs::read_to_string(format!("{BATTERY_DIR}/voltage_now"))
-        .unwrap()
+        .parse::<f64>()?;
+    let voltage_now = fs::read_to_string(format!("{BATTERY_DIR}/voltage_now"))?
         .trim()
-        .parse::<f64>()
-        .unwrap();
+        .parse::<f64>()?;
     print!("{:.1}", (current_now * voltage_now) / 1000000000000.);
+    Ok(())
 }
