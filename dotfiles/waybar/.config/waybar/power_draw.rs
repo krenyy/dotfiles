b317e2c2f0ce858e82664346f1d17dc1d7f29477
Martin Kröner <work@kroner.dev>
2024-09-04 21:08:56 +0200

    waybar: power_draw.rs: another cleanup

--- dotfiles/waybar/.config/waybar/power_draw.rs
+++ dotfiles/waybar/.config/waybar/power_draw.rs
@@ -1,15 +1,27 @@
 #!/usr/bin/env rust-script
 
+//! ```cargo
+//! [dependencies]
+//! anyhow = "1"
+//! ```
+
 use std::fs;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const BATTERY_DIR: &str = "/sys/class/power_supply/BAT0";
-    let current_now = fs::read_to_string(format!("{BATTERY_DIR}/current_now"))?
+use anyhow::Context as _;
+
+const BATTERY_DIR: &str = "/sys/class/power_supply/BAT0";
+
+fn f64_from_file(path: &str) -> anyhow::Result<f64> {
+    fs::read_to_string(path)?
         .trim()
-        .parse::<f64>()?;
-    let voltage_now = fs::read_to_string(format!("{BATTERY_DIR}/voltage_now"))?
-        .trim()
-        .parse::<f64>()?;
+        .parse::<f64>()
+        .context("failed to parse")
+}
+
+fn main() -> anyhow::Result<()> {
+    let current_now = f64_from_file(&format!("{BATTERY_DIR}/current_now"))?;
+    let voltage_now = f64_from_file(&format!("{BATTERY_DIR}/voltage_now"))?;
     print!("{:.1}", (current_now * voltage_now) / 1000000000000.);
+
     Ok(())
 }
