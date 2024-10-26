faac62c5fa167547fd928d6af0d3cd1ecdb09bb4
Martin Kröner <work@kroner.dev>
2024-10-26 13:24:02 +0200

    fish: change `ANDROID_HOME` to `ANDROID_USER_HOME`

--- dotfiles/fish/.config/fish/conf.d/01-env-variables.fish
+++ dotfiles/fish/.config/fish/conf.d/01-env-variables.fish
@@ -25,7 +25,7 @@ set -x XDG_DATA_HOME "$HOME/.local/share"
 set -x XDG_STATE_HOME "$HOME/.local/state"
 
 # clean up $HOME
-set -x ANDROID_HOME "$XDG_DATA_HOME/android"
+set -x ANDROID_USER_HOME "$XDG_DATA_HOME/android"
 set -x CARGO_HOME "$XDG_DATA_HOME/cargo"
 set -x CUDA_CACHE_PATH "$XDG_CACHE_HOME/nv"
 set -x DOT_SAGE "$XDG_CONFIG_HOME/sage"
