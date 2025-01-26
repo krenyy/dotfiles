bced8e1e724dd2bee521dd33f1c2ea374a22d28c
Martin Kröner <work@kroner.dev>
2025-01-26 13:26:23 +0100

    fish: add `git-cleanup-branches` function

--- dotfiles/fish/.config/fish/conf.d/99-interactive.fish
+++ dotfiles/fish/.config/fish/conf.d/99-interactive.fish
@@ -23,6 +23,15 @@ if status is-interactive
         python -c "import sys, urllib.parse as ul; [sys.stdout.write(ul.quote_plus(l)) for l in sys.stdin]"
     end
 
+    function git-cleanup-branches
+        git fetch -p &&
+            git branch -r |
+                awk '{print $1}' |
+                grep -Evf /dev/fd/0 (git branch -vv | grep -F origin | psub) |
+                awk '{print $1}' |
+                xargs git branch -d
+    end
+
     set -x NMAP_PRIVILEGED
 
     if command -v pazi &>/dev/null
